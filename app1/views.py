3d29d0afaeccb8059f70923f5f0e6326152d1a36
Munishr143 <mm1437799@gmail.com>
2023-03-28 00:51:24 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Jaddu(request):
+    return render(request, 'Jaddu.html')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Stokes(request):
+    return render(request, 'Stokes.html')
